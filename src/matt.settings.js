e7355d3c5e92499fbead6cc8c76c2504b7b7ca6c
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-13 06:33:03 -0300

    update settings

--- src/matt.settings.js
+++ src/matt.settings.js
@@ -1,4 +1,58 @@
 const settings = {
+    'about':{
+        'name':'matt-template',
+        'description':`
+            I've made this app as a Template for my own projects.`,
+        },
+        'documentation':{    
+            'directories':[
+                {
+                    'title':'assets',
+                    'descripton':`
+                        All the assets used in this project. 
+                    `
+                },
+                {
+                    'title':'components',
+                    'descripton':`
+                        All the React componentes of this project. 
+                        there are two types of components: styleguide and this-project component.
+                        each one will be introduced in this article.
+                    `
+                },
+                {
+                    'title':'components/styleguide',
+                    'descripton':`
+                        I use my own style system based on Material Design,
+                         a design system created by Google.
+                    `
+                },
+                {
+                    'title':'components/this-project',
+                    'descripton':`
+                        Inside this-project folder there are all the components built exclusively for this project. 
+                    `
+                },
+                {
+                    'title':'context',
+                    'descripton':`
+                        All the contexts and providers. 
+                    `
+                },
+                {
+                    'title':'pages',
+                    'descripton':`
+                        All the pages components. 
+                    `
+                },
+                {
+                    'title':'services',
+                    'descripton':`
+                        inside this folder there are the outside apis configurations. 
+                    `
+                },
+            ]
+    },
     author:{
         'name':'Mateus Faustino',
         'description':'',
@@ -30,11 +84,12 @@ const settings = {
         {
             title:'settings',
             link:'/settings'   
-        }
+        },
+        {
+            title:'other',
+            link:'/other'   
+        },
     ],
-    'about':{
-        
-    }    
   }
   
   export const menu = settings.menu 
